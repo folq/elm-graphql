2374e1369826f7d25dc97391bd139da3df062895
Øystein Hovind <oystein.hovind@folq.no>
2019-12-20 11:44:28 +0100

    Stop depending on `npx`
    In order to make the cli work with `yarn`. There are probably better
    ways to do this, but this is an attempt.

--- generator/src/formatted-write.ts
+++ generator/src/formatted-write.ts
@@ -2,5 +2,5 @@ import { execSync } from "child_process";
 import * as fs from "fs-extra";
 
 export const applyElmFormat = (fileOrFolderToFormat: string): void => {
-  execSync(`npx elm-format --elm-version=0.19 --yes ${fileOrFolderToFormat}/`);
+  execSync(`./node_modules/elm-format/bin/elm-format --elm-version=0.19 --yes ${fileOrFolderToFormat}/`);
 };
